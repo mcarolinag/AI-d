8150a456a80cff81e5716574b1cf0bca564e926e
mcarolinag <mcarolinagc@hotmail.com>
2025-06-29 21:29:25 -0700

    function added to main

--- Front_End/main.py
+++ Front_End/main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI                  # For building the web API
 from fastapi.middleware.cors import CORSMiddleware  # To allow communication between frontend and backend
 from pydantic import BaseModel               # To define the expected format of incoming JSON data
 
+#imports for closest_projects
+import torch
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import geopy.distance
+
 # Create a FastAPI instance
 app = FastAPI()
 
@@ -39,6 +45,49 @@ def input_check(input: DescriptionInput):
         result["Radius"] = input.radius
 
     return result
+
+
+def closest_projects(input: DescriptionInput):
+    query_corpus = ('Project Title: ' + input.title + ' ' + 'Description: ' + input.description
+                    + ' Province: ' + input.province)
+    embedder = SentenceTransformer("intfloat/multilingual-e5-large-instruct")
+    corpus_embeddings = torch.load('corpus_embeddings.pt')
+    top_k = min(input.num_projects, len(corpus_embeddings))
+    query_embedding = embedder.encode(query_corpus, convert_to_tensor=True)
+    max_distance = input.radius
+
+    file_path = 'OECD_Project_Data_Final(6.1).xlsx'
+    all_projects = pd.read_excel(file_path)
+
+    if input.disable_radius:
+
+        # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+        # We use cosine-similarity and torch.topk to find the highest 5 scores
+        similarity_scores = embedder.similarity(query_embedding, corpus_embeddings)[0]
+        scores, indices = torch.topk(similarity_scores, k=top_k)
+        results = pd.DataFrame(list(zip(indices.tolist(), scores.tolist())), columns=['index', 'score'])
+        results_info = pd.merge(results, all_projects.reset_index(), on=['index'])
+
+    else:
+        coords_file = 'Province_Latitude_Longitude.xlsx'
+        province_coords = pd.read_excel(coords_file)
+        coords_query_ = province_coords[province_coords['Province'] == input.province]
+        coords_query = (coords_query_['Latitude'].values[0], coords_query_['Longitude'].values[0])
+        all_projects['distances'] = all_projects[['Latitude', 'Longitude']].apply(
+            lambda x: geopy.distance.geodesic(coords_query, (x['Latitude'], x['Longitude'])).km, axis=1)
+
+        max_dist_projects = all_projects[all_projects['distances'] <= max_distance].reset_index()
+
+        # We use cosine-similarity and torch.topk to find the highest 5 scores
+        similarity_scores = \
+            embedder.similarity(query_embedding, corpus_embeddings[max_dist_projects['index'].values, :])[0]
+        scores, indices = torch.topk(similarity_scores, k=top_k)
+        results = pd.DataFrame(list(zip(max_dist_projects.iloc[indices.tolist()]['index'].values, scores.tolist())),
+                               columns=['index', 'score'])
+        results_info = pd.merge(results, max_dist_projects, on=['index'])
+
+    return results_info
+
 # Define a POST route (API endpoint) called /wordcount
 # Here the input is the class defined above, (which is the input coming from the frontend)
 # input.description is the description object inside the DescriptionInput class
